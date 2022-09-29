b05a19de67e6fa531716cec2c48546a875e432fe
Rayan <rayanchambet1@gmail.com>
2022-09-29 14:58:29 +0200

    INIT

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Header = () => {
+
+    
+    return(
+        <header>
+
+        </header>
+    )
+}
+export default Header;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Home = () => {
+
+
+
+    return(
+        <section>
+
+        </section>
+    )
+}
+
+
+export default Home;
