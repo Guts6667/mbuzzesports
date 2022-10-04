74f01f877e4681b891f1a90c97feac3508aa1fb4
Rayan <rayanchambet1@gmail.com>
2022-10-04 23:19:40 +0200

    V.0
    Header ✅
    Home 🎯

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-import BurgerBtn from "../BurgerBtn";
+import React, { useState } from "react";
 import './Header.scss'
 
 
 const Header = () => {
-
+    const [isOpen, setIsOpen] = useState(false);
+    function toggleBtn(){
+        let navIcon = document.querySelector('.nav-icon');
+        navIcon.classList.toggle('open')
+        if (navIcon.classList.contains('open')) {
+            setIsOpen(true)
+        } else {
+            setIsOpen(false)
+        }
+    }
     
     return(
         <header>
-            <BurgerBtn />
+            <div className="nav-icon" onClick={toggleBtn}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
             <img src="/assets/MBUZZ-Esports-Logo.svg" alt="Logo MBUZZ Esports" />
             <nav className="nav-desktop">
-                <span>About</span>
-                <span>VR Experience EVA</span>
-                <span>100% Gaming Marketplace</span>
-                <span>Events</span>
-                <span>Services</span>
+                <a href="#about">About</a>
+                <a href="#eva">VR Experience EVA</a>
+                <a href="#marketplace">100% Gaming Marketplace</a>
+                <a href="#events">Events</a>
+                <a href="#services">Services</a>
             </nav>
             <div className="btn btn-dark">
                 <a href="#joinUs" >Join us</a>
             </div>
-            
+            {isOpen && (
+                <section className="section__burgerNav">
+                    <nav>
+                        <a onClick={toggleBtn} href="#about"><span>About</span><img src="/assets/chevron-right-solid.svg" alt="Chevron right" /></a>
+                        <a onClick={toggleBtn} href="#eva"><span>VR Experience EVA</span><img src="/assets/chevron-right-solid.svg" alt="Chevron right" /></a>
+                        <a onClick={toggleBtn} href="#marketplace"><span>100% Gaming Marketplace</span><img src="/assets/chevron-right-solid.svg" alt="Chevron right" /></a>
+                        <a onClick={toggleBtn} href="#events"><span>Events</span><img src="/assets/chevron-right-solid.svg" alt="Chevron right" /></a>
+                        <a onClick={toggleBtn} href="#services"><span>Services</span><img src="/assets/chevron-right-solid.svg" alt="Chevron right" /></a>
+                    </nav>
+            </section>
+            )}
         </header>
     )
 }
