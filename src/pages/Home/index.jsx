df933c925e5934a801487cf25453f89abc673cd3
Rayan <rayanchambet1@gmail.com>
2022-10-05 01:59:31 +0200

    V.0.1.1
    Section About 🎯
    OurCompany ✅
    OurAmbition ✅
    Header ✅
    Section HeroHome ✅

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import About from "../../components/About";
+// import About from "../../components/About";
 import HeroHome from "../../components/HeroHome";
 import OurAmbition from "../../components/OurAmbition";
 import OurCompany from "../../components/OurCompany";
