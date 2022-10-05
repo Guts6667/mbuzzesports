000581174b502ebe235b3021d416c6854e48aab5
Rayan <rayanchambet1@gmail.com>
2022-10-06 01:31:37 +0200

    V.0.2.2
    Gaming Experience 🎯
    Fixed issue with deployment
    Footer ✅
    Homepage ✅
    OurRoadmap ✅
    OurTeam ✅
    OurCompany ✅
    OurAmbition ✅
    Header ✅
    Section HeroHome ✅

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,42 +6,36 @@ const Footer = () => {
     return(
         <footer className="container">
             <div className="container__footer-socials">
-                <a href="">
+                {/* <a href="">
                     <img src="/assets/icon-tiktok.svg" alt="icon tiktok" />
-                </a>
-                <a href="">
+                </a> */}
+                <a href="https://www.instagram.com/mbuzztech/">
                     <img src="/assets/icon-instagram.svg" alt="icon instagram" />
                 </a>
-                <a href="">
+                <a href="https://twitter.com/mbuzztech">
                     <img src="/assets/icon-twitter.svg" alt="icon twitter" />
                 </a>
-                <a href="">
+                <a href="https://www.facebook.com/MBUZZTech/">
                     <img src="/assets/icon-facebook.svg" alt="icon facebook" />
                 </a>
             </div>
             <div className="container__footer-socialsXl">
-                <a href="">
+                <a href="https://www.mbuzztech.com">
                     <img src="/assets/Mbuzz-Logo.svg" alt="Logo Mbuzz" className="iconPartner" />
                 </a>
-                <a href="">
-                    <img src="/assets/Mbuzz-Esports-Logo.svg" alt="Logo Mbuzz Esport" className="iconPartner"/>
-                </a>
-                <a href="">
+                <a href="https://www.opera.com">
                     <img src="/assets/Logo-Opera.svg" alt="Logo Opera" className="iconPartner"/>
                 </a>
-                <a href="">
+                <a href="https://www.opera.com/fr/crypto/next">
                     <img src="/assets/Logo-OperaCrypto.svg" alt="Logo Opera Crypt" className="iconPartner"/>
                 </a>
-                <a href="">
-                    <img src="/assets/icon-tiktok.svg" alt="icon tiktok" className="iconSocial"/>
-                </a>
-                <a href="">
+                <a href="https://www.instagram.com/mbuzztech/">
                     <img src="/assets/icon-instagram.svg" alt="icon instagram" className="iconSocial"/>
                 </a>
-                <a href="">
+                <a href="https://twitter.com/mbuzztech">
                     <img src="/assets/icon-twitter.svg" alt="icon twitter" className="iconSocial" />
                 </a>
-                <a href="">
+                <a href="https://www.facebook.com/MBUZZTech/">
                     <img src="/assets/icon-facebook.svg" alt="icon facebook" className="iconSocial" />
                 </a>
             </div>
